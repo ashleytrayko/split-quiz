94e782b9733fd291281614bcbc5ad8160abfe6c5
고형진 <aion7304@gmail.com>
2022-12-26 17:06:30 +0900

    22-12-26 17:06
    메소드 추가..

--- splitstring/src/main/java/org/example/Main.java
+++ splitstring/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,41 +10,59 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         //String txt = "aaaa,bbbb,\"cc,c\"c\",dd\"ee,f,gg,,ijk,\"l,m\",n";
+        boolean flag = true;
         Scanner sc = new Scanner(System.in);
+
+        // Split(정규표현식)사용
+
         SplitMethod split = new SplitMethod();
-        String input = sc.nextLine();
-        String answer[] = split.splitMethod1(input);
-        System.out.println(answer.length + "개");
-        for(int i = 0; i < answer.length; i++){
-            System.out.println("answer" + i + " : " + answer[i]);
+        while(flag){
+            String input = sc.nextLine();
+            if(input.equals("kill")){
+                flag = false;
+            }
+            String answer[] = split.splitMethod1(input);
+            System.out.println(answer.length + "개");
+            for(int i = 0; i < answer.length; i++){
+                System.out.println("answer " + i + " : " + answer[i]);
+            }
         }
 
+        // 반복문 사용
 
-
-
-    }
-//        String result = "";
-//        char previousChar = ' ';
-//        ArrayList<String> answer = new ArrayList<>();
-//        for (int i = 0; i < txt.length(); i++) {
-//            if (txt.charAt(i) == ',') {
-//                if(previousChar == '\"'){
-//                    answer.add(result);
-//                    result = "";
-//                    continue;
-//                }
-//                answer.add(result);
-//                result = "";
-//                continue;
+//        SplitMethod splitMethod2 = new SplitMethod();
+//         while (flag){
+//             String input = sc.nextLine();
+//              if(input.equals("kill")){
+//                  flag = false;
+//              }
+//              List<String> answer = splitMethod2.splitMethod2(input);
+//              System.out.println(answer.size());
+//              for(int i = 0; i < answer.size(); i++){
+//                    System.out.println("answer " + i + " : " + answer.get(i));
+//              }
+//         }
+        
+        
+        //CSV라이브러리 사용
+//        SplitMethod splitMethod = new SplitMethod();
+//        while (flag) {
+//            String input = sc.nextLine();
+//            if (input.equals("kill")) {
+//                flag = false;
 //            }
-//            result += txt.charAt(i);
-//            previousChar = txt.charAt(i);
-//            if(txt.length() -1 == i){
-//                answer.add(result);
+//            List<String[]> answers = splitMethod.splitMethod4(input);
+//            int listIndex = 0;
+//            for (String[] answer : answers) {
+//                System.out.println(answer.length + "개");
+//                System.out.println("\nString[" + listIndex++ + "] : " + Arrays.toString(answer));
+//
+//                int index = 0;
+//                for (String array : answer) {
+//                    System.out.println(index++ + " : " + array);
+//                }
 //            }
 //        }
-//        System.out.println(answer);
-//
-//    }
+    }
 
 }
--- splitstring/src/main/java/org/example/SplitMethod.java
+++ splitstring/src/main/java/org/example/SplitMethod.java
@@ -1,12 +1,75 @@
 package org.example;
 
+import com.google.common.base.Splitter;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SplitMethod {
     public String[] splitMethod1 (String input){
         // ("가 앞에 존재한다면 false),("가 뒤에 존재한다면 false)
-        String answer[] = input.split("(?<!\\\"\\w),(?!\\w\\\")");
+//        String answer[] = input.split("(?<!\\\"\\w),(?!\\w\\\")");
+
+        // ""가 없거나 복수인 ,를 찾아 자르기
+        String answer[] = input.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+//        String answer[] = input.split("(?!\B(\"|“|”)[^(\"|“|”)]*),(?![^(\"|“|”)]*(\"|“|”)\B)");
         return answer;
     }
+
+    public List<String> splitMethod2 (String input){
+        List<String> answer = new ArrayList<>();
+        String result = "";
+        int startPosition = 0;
+        boolean isInQuotes = false;
+        for(int currentPosition = 0; currentPosition < input.length(); currentPosition++){
+            if (input.charAt(currentPosition) == '\"') {
+                isInQuotes = !isInQuotes;
+            }
+            else if (input.charAt(currentPosition) == ',' && !isInQuotes) {
+                answer.add(input.substring(startPosition, currentPosition));
+                startPosition = currentPosition + 1;
+            }
+        }
+
+        String lastToken = input.substring(startPosition);
+        if (lastToken.equals(",")) {
+            answer.add("");
+        } else {
+            answer.add(lastToken);
+        }
+
+        return answer;
+    }
+    public List<String> splitMethod3 (String input){
+        Pattern pattern = Pattern.compile("\",(?!\\s)\"");
+        Splitter splitter = Splitter.on(pattern);
+        List<String> answers = splitter.splitToList(input);
+        return answers;
+    }
+
+    public List<String[]> splitMethod4(String input){
+        CSVParser parser = new CSVParserBuilder()
+                .withSeparator(',')
+                .build();
+        CSVReader reader = new CSVReaderBuilder(new StringReader(input))
+                .withCSVParser(parser)
+                .build();
+        List<String[]> list = new ArrayList<>();
+        try {
+            list = reader.readAll();
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
 }
